ea4219126b454b419930ee6b1e9645b2fbbcfd43
Roberth <rjromerol1999@gmail.com>
2021-02-10 22:03:38 -0500

    C++ basics

---
+++ basics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+  // Output
+  int count = 10;
+  std::cout << "Hello World\n";
+  std::cout << "The count is in "<< count << " and the max value is 80." << std::endl;
+
+  // Input
+  int userInput;
+  std::cin >> userInput;
+  std::cout << "You entered the number " << userInput << std::endl;
+  
+
+  return 0;
+}
